5e9cbe4207ff9e68b102300e903c625d2d64894d
Ahmad Djunaedi <ahmaddjunaedi92@gmail.com>
2019-01-04 16:50:47 +0700

    timestamps false email

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -6,16 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
+	public $timestamps = false;
+	
 	protected $table = 'email';
 
 	protected $primaryKey = 'EmailId';
 
     protected $fillable = [
-        'Subject', 'To','Body','Cc','Attacment','Campaign','IsUsed'
-    ];
-
-    protected $dates = [
-        'CreatedAt',
-        'UpdatedAt'
+        'Subject', 'To','Body','Cc','Attacment','Campaign','IsUsed',
+        'CreatedAt','UpdatedAt'
     ];
 }
